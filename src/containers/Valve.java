2e4f6803128554e13fa17cbec75d8400344698f8
arthas_sjtu <arthas_sjtu@163.com>
2017-10-13 22:04:49 +0800

    重构了代码，加入了defaultServlet

---
+++ src/containers/Valve.java
@@ -0,0 +1,12 @@
+package containers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by jrj on 17-10-13.
+ */
+public interface Valve {
+    void invoke(HttpServletRequest request, HttpServletResponse response);
+    String getInfo();
+}
